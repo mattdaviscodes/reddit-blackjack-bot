02a2b6ab0d2cc7c1b44c11ef25c24c8344b01acb
Matt Davis <matt@abuvmedia.com>
2017-01-21 22:39:50 -0800

    Add unique IDs to all Card objects. Add encode method to Hand, to allow storage of hands in single string in DB.

--- dealer.py
+++ dealer.py
@@ -108,15 +108,22 @@ class Hand(object):
                 return True
         return False
 
+    def encode_hand_for_db(self):
+        return '|'.join([card.id for card in self.cards])
+
+    def decode_hand_from_db(self):
+        pass
+
     def __repr__(self):
         return "{} - {}".format(self.get_hand_value(), '|'.join([str(card) for card in self.cards]))
 
 
 class Card(object):
-    def __init__(self, suit, symbol, value):
+    def __init__(self, suit, symbol, value, id):
         self.suit = suit  # [C,H,D,S]
         self.symbol = symbol  # [A,2,3,4,5,6,7,8,9,10,J,Q,K]
         self.value = value  # [1,2,3,4,5,6,7,8,9,10,11]
+        self.id = id
 
     def get_ascii_art(self):
         pass
@@ -130,58 +137,58 @@ class Card(object):
 
 class Deck(object):
     def __init__(self):
-        self.deck = [Card('C', 'A', 11),
-                     Card('C', '2', 2),
-                     Card('C', '3', 3),
-                     Card('C', '4', 4),
-                     Card('C', '5', 5),
-                     Card('C', '6', 6),
-                     Card('C', '7', 7),
-                     Card('C', '8', 8),
-                     Card('C', '9', 9),
-                     Card('C', 'T', 10),
-                     Card('C', 'J', 10),
-                     Card('C', 'Q', 10),
-                     Card('C', 'K', 10),
-                     Card('S', 'A', 11),
-                     Card('S', '2', 2),
-                     Card('S', '3', 3),
-                     Card('S', '4', 4),
-                     Card('S', '5', 5),
-                     Card('S', '6', 6),
-                     Card('S', '7', 7),
-                     Card('S', '8', 8),
-                     Card('S', '9', 9),
-                     Card('S', 'T', 10),
-                     Card('S', 'J', 10),
-                     Card('S', 'Q', 10),
-                     Card('S', 'K', 10),
-                     Card('H', 'A', 11),
-                     Card('H', '2', 2),
-                     Card('H', '3', 3),
-                     Card('H', '4', 4),
-                     Card('H', '5', 5),
-                     Card('H', '6', 6),
-                     Card('H', '7', 7),
-                     Card('H', '8', 8),
-                     Card('H', '9', 9),
-                     Card('H', 'T', 10),
-                     Card('H', 'J', 10),
-                     Card('H', 'Q', 10),
-                     Card('H', 'K', 10),
-                     Card('D', 'A', 11),
-                     Card('D', '2', 2),
-                     Card('D', '3', 3),
-                     Card('D', '4', 4),
-                     Card('D', '5', 5),
-                     Card('D', '6', 6),
-                     Card('D', '7', 7),
-                     Card('D', '8', 8),
-                     Card('D', '9', 9),
-                     Card('D', 'T', 10),
-                     Card('D', 'J', 10),
-                     Card('D', 'Q', 10),
-                     Card('D', 'K', 10)]
+        self.deck = [Card('C', 'A', 11, 1),
+                     Card('C', '2', 2, 2),
+                     Card('C', '3', 3, 3),
+                     Card('C', '4', 4, 4),
+                     Card('C', '5', 5, 5),
+                     Card('C', '6', 6, 6),
+                     Card('C', '7', 7, 7),
+                     Card('C', '8', 8, 8),
+                     Card('C', '9', 9,9),
+                     Card('C', 'T', 10, 10),
+                     Card('C', 'J', 10, 11),
+                     Card('C', 'Q', 10, 12),
+                     Card('C', 'K', 10, 13),
+                     Card('S', 'A', 11, 14),
+                     Card('S', '2', 2, 15),
+                     Card('S', '3', 3, 16),
+                     Card('S', '4', 4, 17),
+                     Card('S', '5', 5, 18),
+                     Card('S', '6', 6, 19),
+                     Card('S', '7', 7, 20),
+                     Card('S', '8', 8, 21),
+                     Card('S', '9', 9, 22),
+                     Card('S', 'T', 10, 23),
+                     Card('S', 'J', 10, 24),
+                     Card('S', 'Q', 10, 25),
+                     Card('S', 'K', 10, 26),
+                     Card('H', 'A', 11, 27),
+                     Card('H', '2', 2, 28),
+                     Card('H', '3', 3, 29),
+                     Card('H', '4', 4, 30),
+                     Card('H', '5', 5, 31),
+                     Card('H', '6', 6, 32),
+                     Card('H', '7', 7, 33),
+                     Card('H', '8', 8, 34),
+                     Card('H', '9', 9, 35),
+                     Card('H', 'T', 10, 36),
+                     Card('H', 'J', 10, 37),
+                     Card('H', 'Q', 10, 38),
+                     Card('H', 'K', 10, 39),
+                     Card('D', 'A', 11, 40),
+                     Card('D', '2', 2, 41),
+                     Card('D', '3', 3, 42),
+                     Card('D', '4', 4, 43),
+                     Card('D', '5', 5, 44),
+                     Card('D', '6', 6, 45),
+                     Card('D', '7', 7, 46),
+                     Card('D', '8', 8, 47),
+                     Card('D', '9', 9, 48),
+                     Card('D', 'T', 10, 49),
+                     Card('D', 'J', 10, 50),
+                     Card('D', 'Q', 10, 51),
+                     Card('D', 'K', 10, 52)]
 
     def shuffle(self):
         shuffle(self.deck)
